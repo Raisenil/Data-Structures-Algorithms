a902e17dbcbff51a38971abdd7d25ed77015ec5b
Raisenil <raisulislamniloy47@gmail.com>
2021-06-13 11:34:34 +0600

    recursion

---
+++ Recursion/Nested Recursion/main.c
@@ -0,0 +1,21 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int fun(int n)
+{
+    if (n > 100)
+    {
+        return n-10;
+    }
+    return fun(fun(n+11));
+}
+
+int main()
+{
+    int r;
+    r=fun(95);
+    printf("%d\n",r);
+    return 0;
+}
---
+++ Recursion/Sum if First 'n' Natural Numbers/main.c
@@ -0,0 +1,11 @@
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+int main()
+{
+    return 0;
+}
